4ab5e486d239e33dd6d72cfd80947cbbec13bc20
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-09-17 11:03:04 +0800

    ok

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -1,24 +1,32 @@
 import UserTopNav from "../components/userComponents/UserTopNav";
 
 export default function HomePage() {
-  var self = 100;
-  self = self - 50;
-  self = self - 50;
-  self = self - 50;
-  var filthy = "ewrwe";
-  {
-    if (filthy) {
-      console.log("filthy animal");
-    } else {
-      console.log("not filthy animal");
-    }
+  let x = 68;
+  let y = 100;
+  let chmabaNumber1 = 69;
+  let chmabaNumber2 = 101;
+
+  if (x === chmabaNumber1 && y === chmabaNumber2) {
+    console.log("chamba choy");
+    document.getElementsByClassName("lol").innerHTML = "chamba choy";
+  } else if (x === chmabaNumber1 || y === chmabaNumber2) {
+    console.log("chamba isa choy");
+    document.getElementsByClassName("lol").innerHTML = "chamba isa choy";
+  } else {
+    console.log(!"wala2x gid chamba");
+    document.getElementsByClassName("lol").innerHTML = "wala2x gid chamba";
   }
-  console.log(self);
+
   return (
     <div className="">
       <UserTopNav></UserTopNav>
       <div className="p-5">
         <h1>List</h1>
+        <div className=" grid gap-1">
+          <input type="text" id="firstInput" />
+          <input type="text" id="secondInput" />
+          <input type="text" readOnly id="output" />
+        </div>
         <button
           className=" border border-solid bg-red-400 hover:bg-slate-500 p-2 hover:text-white rounded-full h-full"
           onClick={null}
