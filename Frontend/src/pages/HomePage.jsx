0553f28748e1dd528e2f09b1e8bc91756c6bc673
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-09-18 10:00:56 +0800

    ok

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -7,20 +7,10 @@ function HomePage() {
     <div className="">
       <UserTopNav></UserTopNav>
       <div className="p-5">
-        <h1>List</h1>
-        <div className=" grid gap-1">
-          <input type="text" id="firstInput" />
-          <input type="text" id="secondInput" />
-          <input type="text" readOnly id="output" />
-        </div>
-        <button
-          className=" border border-solid bg-red-400 hover:bg-slate-500 p-2 hover:text-white rounded-full h-full"
-          onClick={null}
-        >
+        <h1>hello</h1>
+        <button className="hover:bg-slate-950 text-white rounded-full p-3 bg-slate-500">
           click
         </button>
-        <h1 className="lol">lol</h1>
-        <ul>{null}</ul>
       </div>
     </div>
   );
