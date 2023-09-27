b4e01954bd18a6d97c8fd79774ffa66d85f34ae7
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-09-27 17:01:52 +0800

    ok

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,12 +2,14 @@ import UserTopNav from "../components/userComponents/UserTopNav";
 
 function HomePage() {
   //
+  const text = document.querySelector(".lol");
+  text.style.color = "red";
 
   return (
     <div className="">
       <UserTopNav></UserTopNav>
       <div className="p-5">
-        <h1>hello</h1>
+        <h1 className="lol">hello</h1>
         <button className="hover:bg-slate-950 text-white rounded-full p-3 bg-slate-500">
           click
         </button>
