7fd6ace8794ba715a6d4387256eeae0e54138e41
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-02 11:17:59 +0800

    ok

--- Frontend/src/pages/LoginPage.jsx
+++ Frontend/src/pages/LoginPage.jsx
@@ -10,21 +10,21 @@ const LoginPage = () => {
   };
 
   return (
-    <div className=" flex justify-center flex-col items-center h-screen ">
-      <div className="flex bg-gray-900 border border-gray-700 p-4 m-3  rounded-md  flex-col md:w-80 ">
+    <div className=" flex justify-center flex-col items-center h-screen text-white">
+      <div className="flex bg-gray-900 border border-gray-700 p-4 m-2  rounded-md  flex-col md:w-80 ">
         <div className=" flex justify-center">
           <Link className="flex place-content-stretch" to="/loginPage">
             <img className=" w-8 h-8" src="/favicon2.svg" alt="logo" />
-            <p className="text-white font-semibold text-2xl pl-1">TEC</p>
+            <p className=" font-semibold text-2xl pl-1">TEC</p>
           </Link>
         </div>
-        <h1 className="flex justify-center text-center text-white font-semibold text-2xl mb-3 ">
+        <h1 className="flex justify-center text-center  font-semibold text-2xl mb-3 ">
           Sign in to your account
         </h1>
         <form onSubmit={handleSubmit}>
-          <p className=" text-white text-xs pb-1 ">Your username</p>
+          <p className="  text-xs pb-1 ">Your username</p>
           <input
-            className=" rounded-md bg-gray-700/25 text-white p-2 text-xs w-full mb-4 outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            className=" rounded-md bg-gray-700/25  p-2 text-xs w-full mb-4 outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
             type="text"
             name="username"
             placeholder="username"
@@ -34,9 +34,9 @@ const LoginPage = () => {
             value={username}
             onChange={(e) => setUsername(e.target.value)}
           />
-          <p className=" text-white text-xs pb-1">Password</p>
+          <p className="  text-xs pb-1">Password</p>
           <input
-            className=" rounded-md bg-gray-700/25 text-white p-2 text-xs w-full mb-4 outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+            className=" rounded-md bg-gray-700/25  p-2 text-xs w-full mb-4 outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
             type="password"
             name="password"
             placeholder="••••••••"
@@ -49,12 +49,12 @@ const LoginPage = () => {
           <div className="md:flex md:justify-between">
             <div className=" flex items-start pb-1">
               <input
-                className=" w-4 h-4"
+                className=" w-4 h-4 "
                 type="checkbox"
                 name="checkbox"
                 title="Click to show/hide password"
               />
-              <p className=" text-white text-xs pl-1">Show password</p>
+              <p className="  text-xs pl-1">Show password</p>
             </div>
             <Link to="#">
               <p className=" text-xs text-sky-500 hover:underline pb-3">
@@ -63,21 +63,18 @@ const LoginPage = () => {
             </Link>
           </div>
           <button
-            className=" text-white bg-gradient-to-bl from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-sm p-2 rounded-lg w-full hover:from-pink-500 hover:via-purple-500 hover:to-yellow-500 mb-3"
+            className="  bg-gradient-to-bl from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-sm p-2 rounded-lg w-full hover:from-pink-500 hover:via-purple-500 hover:to-yellow-500 mb-3"
             type="submit"
           >
             Sign in
           </button>
           <div className="sm:flex ">
-            <p className=" text-sm text-white">
-              Don&apos;t have an account yet?
-            </p>
+            <p className=" text-sm ">Don&apos;t have an account yet?</p>
             <Link
               className=" text-sm text-sky-500 hover:underline sm:pl-1"
-              to="#"
+              to="/registerPage"
             >
-              {" "}
-              Sign up
+              Register here
             </Link>
           </div>
         </form>
--- Frontend/src/pages/RegisterPage.jsx
+++ Frontend/src/pages/RegisterPage.jsx
@@ -1,34 +1,64 @@
-// import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const RegisterPage = () => {
-  // const [name, setName] = useState(["cat", "dog", "cow"]);
-  let admin = false;
-
-  // const [i, setI] = useState(0);
-
-  // const fx = () => {
-  //   setI(i + 1);
-  // };
-  // useEffect(() => {
-  //   console.log(i);
-  // });
-  // let age = 1;
-  // let name = age < 20 ? "young" : age === 20 ? "same age" : "old";
-  // console.log(name);
-
-  const animals = ["dog", "cat", "goat"];
-
-  animals.map((e) => {
-    return <h1>{e} </h1>;
-  });
-
-  return admin === true ? (
-    <>
-      <h1 className=" text-white">{e} Render Old</h1>
-    </>
-  ) : (
-    <>
-      <h1 className=" text-white">Render young</h1>
-    </>
+  return (
+    <div className=" flex justify-center  h-screen items-center text-white">
+      <div className=" mx-auto bg-gray-900 border border-gray-700 m-2  rounded-md p-4 w-60 sm:w-full sm:mx-3 ">
+        <div className=" flex justify-center">
+          <Link className="flex place-content-stretch" to="/loginPage">
+            <img className=" w-8 h-8" src="/favicon2.svg" alt="logo" />
+            <p className=" font-semibold text-2xl pl-1">TEC</p>
+          </Link>
+        </div>
+        <h1 className="flex justify-center text-center  font-semibold text-2xl mb-6 ">
+          Register an account
+        </h1>
+        <form
+          className="grid gap-2 sm:grid sm:grid-cols-2 sm:gap-x-3"
+          action=""
+        >
+          <div>
+            <label htmlFor="fullName">Full name</label>
+            <input
+              className=" rounded-md bg-gray-700/25  p-2 text-xs w-full  outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+              type="text"
+              name="full-name"
+              id="fullName"
+              placeholder="Full name"
+              title="Enter your Full name"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="address">Address</label>
+            <textarea
+              className=" rounded-md bg-gray-700/25  p-2 text-xs w-full  outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500 resize-none focus:h-24 overflow-hidden focus:overflow-visible scrollbar-thin"
+              type="text"
+              name="address"
+              id="address"
+              placeholder="address"
+              title="Enter your Address"
+              autoComplete="off"
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor="birthDay">Birthday</label>
+            <input
+              className=" rounded-md bg-gray-700/25  p-2 text-xs w-full  outline-none border border-gray-600 focus:border-sky-500 focus:ring-1 focus:ring-sky-500"
+              type="date"
+              name="birthDay"
+              id="birthDay"
+              placeholder="Full name"
+              title="Enter your Birthday"
+              autoComplete="off"
+              required
+            />
+          </div>
+        </form>
+      </div>
+    </div>
   );
 };
 
