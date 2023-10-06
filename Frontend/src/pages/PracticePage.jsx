9e64f61d423c40fc345410af91b02eed15e237cd
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-06 12:17:10 +0800

    ok

--- Frontend/src/pages/PracticePage.jsx
+++ Frontend/src/pages/PracticePage.jsx
@@ -1,31 +1,19 @@
-import { PropTypes } from "prop-types";
+import { useState } from "react";
 const PracticePage = () => {
-  return (
-    <div className="text-white grid justify-center h-screen items-center bg-slate-700">
-      <h1>
-        {users.map((user, key) => {
-          return <User name={user.name} age={user.age} key={key}></User>;
-        })}
-      </h1>
-    </div>
-  );
-};
-const users = [
-  { name: "bukNoylol", age: 69 },
-  { name: "mirAng", age: 25 },
-  { name: "Filthy", age: 20 },
-];
+  let [textColor, setTextColor] = useState("green");
 
-const User = (props) => {
   return (
-    <div>
-      {props.name} {props.age}
+    <div className=" text-white">
+      <h1 style={{ color: textColor }}>BukNoy</h1>
+      <button
+        onClick={() => {
+          setTextColor(textColor === "green" ? "violet" : "green");
+        }}
+      >
+        click
+      </button>
     </div>
   );
-};
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number,
 };
 
 export default PracticePage;
