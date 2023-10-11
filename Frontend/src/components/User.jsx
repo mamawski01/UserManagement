4feb29aa9f18ae70391c3418417850e34b42c2a0
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-11 11:13:21 +0800

    ok

--- Frontend/src/components/User.jsx
+++ Frontend/src/components/User.jsx
@@ -1,29 +1,7 @@
-const User = (p) => {
+const User = () => {
   return (
-    <div className="flex gap-x-2">
-      <p
-        style={
-          p.done ? { backgroundColor: "green" } : { backgroundColor: "inherit" }
-        }
-      >
-        {p.name}
-      </p>
-      <p> {p.id}</p>
-      <p>{p.done.toString()} </p>
-      <button
-        onClick={() => {
-          p.deleteTask(p.id);
-        }}
-      >
-        x
-      </button>
-      <button
-        onClick={() => {
-          p.completeTask(p.id);
-        }}
-      >
-        done
-      </button>
+    <div className=" text-violet-500">
+      <div className="">lol</div>
     </div>
   );
 };
--- Frontend/src/pages/PracticePage.jsx
+++ Frontend/src/pages/PracticePage.jsx
@@ -1,56 +1,16 @@
-import { useState } from "react";
-import User from "../components/User";
 const PracticePage = () => {
-  const [newTask, setNewTask] = useState("");
-  const [taskList, setTaskList] = useState([]);
+  const p = new Promise((fullFilled, notFullFilled) => {
+    let a = 1 + 1;
+    a == 2 ? fullFilled("success") : notFullFilled("failed");
+  });
 
-  const inputTask = (event) => {
-    setNewTask(event.target.value);
-  };
-  const addTask = () => {
-    const newTaskWithId = {
-      id: taskList.length === 0 ? 0 : taskList[taskList.length - 1].id + 1,
-      name: newTask,
-      done: false,
-    };
-    setTaskList([...taskList, newTaskWithId]);
-  };
-  const deleteTask = (id) => {
-    setTaskList(
-      taskList.filter((v) => {
-        return v.id === id ? false : true;
-      })
-    );
-  };
-  const completeTask = (id) => {
-    setTaskList(
-      taskList.map((v) => {
-        return v.id === id ? { ...v, done: v.done ? false : true } : v;
-      })
-    );
-  };
-
+  p.then((message) => {
+    console.log("this is in the then " + message);
+  });
   return (
     <div className=" text-white">
-      <input onChange={inputTask} type="text" />
-      <div className=" flex gap-x-3">
-        <button onClick={addTask}>click</button>
-        {newTask}
-      </div>
-      <div className="">
-        {taskList.map((v, k) => {
-          return (
-            <User
-              key={k}
-              done={v.done}
-              name={v.name}
-              id={v.id}
-              deleteTask={deleteTask}
-              completeTask={completeTask}
-            ></User>
-          );
-        })}
-      </div>
+      <button> Generate cat ../..</button>
+      <div className=""></div>
     </div>
   );
 };
