b4a8434b9484676ff677bda0082150cec6a98d78
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-15 16:45:10 +0800

    ok

--- Frontend/src/pages/PracticePage.jsx
+++ Frontend/src/pages/PracticePage.jsx
@@ -1,41 +1,18 @@
-import Axios from "axios";
 import { useState } from "react";
-const Call = () => {
-  const [reason, setReason] = useState("");
 
-  const fetchData = (excuse) => {
-    Axios.get(`https://excuser-three.vercel.app/v1/excuse/${excuse}/`).then(
-      (res) => {
-        setReason(res.data[0]);
-      }
-    );
+const Call = () => {
+  const [show, setShow] = useState(false);
+
+  const reveal = () => {
+    setShow(!show);
   };
 
   return (
-    <div className=" text-white flex flex-col">
-      <button
-        onClick={() => {
-          fetchData("party");
-        }}
-      >
-        party
-      </button>
-      <button
-        onClick={() => {
-          fetchData("gaming");
-        }}
-      >
-        gaming
-      </button>
-      <button
-        onClick={() => {
-          fetchData("unbelievable");
-        }}
-      >
-        unbelievable
-      </button>
-      <p>Category: {reason.category}</p>
-      <p>Reason: {reason.excuse}</p>
+    <div className=" text-white">
+      <div className="">
+        {show ? "visible" : ""}
+        <button onClick={reveal}> click</button>
+      </div>
     </div>
   );
 };
