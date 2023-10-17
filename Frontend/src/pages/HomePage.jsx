bfbd54d971e85abe2a5f57311c00bc3a4bb3f2a3
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-17 11:38:33 +0800

    ok

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -2,13 +2,10 @@ import UserTopNav from "../components/userComponents/UserTopNav";
 
 const HomePage = () => {
   return (
-    <div className="">
+    <div className=" text-white">
       <UserTopNav></UserTopNav>
       <div className="p-5">
         <h1 className="lol">hello</h1>
-        <button className="hover:bg-slate-950 text-white rounded-full p-3 bg-slate-500">
-          click
-        </button>
       </div>
     </div>
   );
