16866e4e6203333b6ed4dbe9eb69aee29ddd6fe1
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-19 12:34:09 +0800

    try

--- Frontend/src/pages/PracticePage.jsx
+++ Frontend/src/pages/PracticePage.jsx
@@ -1,26 +1,5 @@
-import { useState } from "react";
+import React from "react";
 
-const Call = () => {
-  const [PHPValue, setPHPValue] = useState(0);
-
-  const convertToUSD = () => {
-    setPHPValue(PHPValue * 0.0176);
-  };
-
-  return (
-    <div className=" text-white">
-      <div className=" w-56">
-        <input
-          type="text"
-          onChange={(event) => {
-            setPHPValue(event.target.value);
-          }}
-        />
-        <button onClick={convertToUSD}>to USD</button>
-        <input type="text" readOnly value={PHPValue} />
-      </div>
-    </div>
-  );
-};
-
-export default Call;
+export default function PracticePage() {
+  return <div></div>;
+}
