3cd7652bd8ee1e86f8319d9a78706926c2efaa8f
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-20 14:21:25 +0800

    Update App.jsx
    ok

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { useRoutes, Outlet } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import UserPutPage from "./pages/UserPutPage";
 import LoginPage from "./pages/LoginPage";
@@ -6,21 +6,40 @@ import RegisterPage from "./pages/RegisterPage";
 import PracticePage from "./pages/PracticePage";
 
 const App = () => {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<HomePage></HomePage>}></Route>
-        <Route path="*" element={<h1>Page Not Found</h1>}></Route>
-        <Route path="/userPut" element={<UserPutPage></UserPutPage>}></Route>
-        <Route path="/loginPage" element={<LoginPage></LoginPage>}></Route>
-        <Route
-          path="/registerPage"
-          element={<RegisterPage></RegisterPage>}
-        ></Route>
-        <Route path="/practice" element={<PracticePage></PracticePage>}></Route>
-      </Routes>
-    </>
-  );
+  const routes = useRoutes([
+    {
+      path: "/",
+      element: (
+        <div className="">
+          <HomePage></HomePage> <Outlet></Outlet>
+        </div>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <div className="">Home</div>,
+        },
+        {
+          path: "/loginPage",
+          element: <LoginPage></LoginPage>,
+        },
+        {
+          path: "/userPutPage",
+          element: <UserPutPage></UserPutPage>,
+        },
+        {
+          path: "/registerPage",
+          element: <RegisterPage></RegisterPage>,
+        },
+        {
+          path: "/practicePage",
+          element: <PracticePage></PracticePage>,
+        },
+      ],
+    },
+  ]);
+
+  return routes;
 };
 
 export default App;
