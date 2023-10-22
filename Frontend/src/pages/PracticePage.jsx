f6f89a5ec8aab2fe5b317fcb7cd3c02cd65ff869
mamawski01 <72120572+mamawski01@users.noreply.github.com>
2023-10-22 10:03:29 +0800

    ok

--- Frontend/src/pages/PracticePage.jsx
+++ Frontend/src/pages/PracticePage.jsx
@@ -1,5 +1,16 @@
-import React from "react";
+import { useState } from "react";
 
 export default function PracticePage() {
-  return <div></div>;
+  const [name, setName] = useState(true);
+
+  const onClick = () => {
+    setName(!name);
+  };
+
+  return (
+    <div className=" text-white">
+      <div className="">Name:{name ? "BukNoy" : "mirAng the Cat"}</div>
+      <button onClick={onClick}> click</button>
+    </div>
+  );
 }
